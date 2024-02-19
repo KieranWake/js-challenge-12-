83ee1808e2a7b7e35989a45e7a8d17914ff7a234
Kieran Wake <kieranwake1234@gmail.com>
2024-02-19 00:08:24 +0000

    wrote some guidelines for solution

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function numberSquares(n) {
+    //conditional statement to compare number of squares in previous squares -- 
+    // and the value of the new square. 
+    // use math.pow to find the power of, for the next sequential square. 
+    // What method can we use to find the previous number of squares from the one --
+    //prior.
 	
 }
